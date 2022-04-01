cd091e7ba9117eb25e1f256bf26d4e6fa917ef6f
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-04-01 14:20:26 -0700

    adding link to all posts created by clicked user

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const UserItem = (prop) => {
   return (
     <li>
       <div>
-        <img src={prop.image} alt={prop.id}></img>
-        <h2>{prop.name}</h2>
-        <h3>{prop.placeCount} {prop.placeCount>1? 'places': 'place'}</h3>
+        <Link to={`/${prop.id}/places`}>
+          <img src={prop.image} alt={prop.id}></img>
+          <h2>{prop.name}</h2>
+          <h3>{prop.placeCount} {prop.placeCount>1? 'places': 'place'}</h3>
+        </Link>
       </div>
     </li>
   )
