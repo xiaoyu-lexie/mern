aec6bca2aa13f4bc77d952bf2d540279052d953a
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-04-02 11:51:18 -0700

    adding sidebrawer for phone viewing

--- frontend/src/shared/components/Navigation/MainNagigation.js
+++ frontend/src/shared/components/Navigation/MainNagigation.js
@@ -5,24 +5,32 @@ import './MainNavigation.css';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 
 const MainNagigation = () => {
   return (
-    <MainHeader>
-      <button className='main-navigation__menu-btn'>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className='main-navigation__title'>
-        <Link to='/'>
-        Your places
-        </Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className='main-navigation__drawer-nav'>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className='main-navigation__menu-btn'>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className='main-navigation__title'>
+          <Link to='/'>
+          Your places
+          </Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   )
 };
 
