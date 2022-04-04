c3f3d5df88f5b9bed0f436226bb20d3d402f4765
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-04-04 09:56:45 -0700

    hide side drawer right after click side drawer

--- frontend/src/shared/components/Navigation/MainNagigation.js
+++ frontend/src/shared/components/Navigation/MainNagigation.js
@@ -11,26 +11,26 @@ import Backdrop from '../UIElements/Backdrop';
 const MainNagigation = () => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  const openDrawer = () => {
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
   };
 
-  const closeDrawer = () => {
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
   }
 
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
       {drawerIsOpen &&
-      <SideDrawer>
+      <SideDrawer onClick={closeDrawerHandler}>
         <nav className='main-navigation__drawer-nav'>
           <NavLinks />
         </nav>
       </SideDrawer>
       }
       <MainHeader>
-        <button className='main-navigation__menu-btn' onClick={openDrawer}>
+        <button className='main-navigation__menu-btn' onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
