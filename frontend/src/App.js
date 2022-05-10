5631aa51953f6fe3f025eac391de7ebb4038faaa
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-05-09 18:27:51 -0700

    complete authentication frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import React, {useState, useCallback} from 'react';
+
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
@@ -6,33 +8,74 @@ import NewPlace from './places/pages/NewPlace';
 import MainNagigation from './shared/components/Navigation/MainNagigation';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import {AuthContext } from './shared/context/auth-context';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true)
+  }, [])
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false)
+  }, [])
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+
+        <Route path='/places/new'>
+          <NewPlace />
+        </Route>
+
+        <Route path='/places/:placeId'>
+          <UpdatePlace />
+        </Route>
+
+        <Route path='/:userId/places' exact>
+          <UserPlaces />
+        </Route>
+
+        <Redirect to='/' />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+
+        <Route path='/auth'>
+          <Auth />
+        </Route>
+
+        <Route path='/:userId/places' exact>
+          <UserPlaces />
+        </Route>
+
+        <Redirect to='/auth' />
+      </Switch>
+    );
+  }
+
   return (
-    <Router>
-      <main>
-        <MainNagigation />
-        <Switch>
-          <Route path='/' exact>
-            <Users />
-          </Route>
-
-          <Route path='/places/new'>
-            <NewPlace />
-          </Route>
-
-          <Route path='/places/:placeId'>
-            <UpdatePlace />
-          </Route>
-
-          <Route path='/:userId/places' exact>
-            <UserPlaces />
-          </Route>
-
-          <Redirect to='/' />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider value={{
+      isLoggedIn:isLoggedIn, login: login, logout: logout
+    }} >
+      <Router>
+        <main>
+          <MainNagigation />
+            {routes}
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
